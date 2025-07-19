d245c7cc61f10cad0445b7cb4e6b7873e42150ce
Ricc <ricochristian707@gmail.com>
2025-07-20 00:35:42 +0800

    added a lot of animation

---
+++ app.js
@@ -0,0 +1,11 @@
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 5,
+    freeMode: true,
+    loop: true,
+    spaceBetween: 44,
+    freeMode: true,
+    speed: 6500,
+    autoplay: {
+        delay: 0,
+    },
+});

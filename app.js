4248f7f6b70656d45d3951f3856557b55540cc5a
Ricc <ricochristian707@gmail.com>
2025-07-30 11:46:25 +0800

    minor changes

--- app.js
+++ app.js
@@ -1,5 +1,17 @@
 const swiper = new Swiper('.swiper', {
-    slidesPerView: 5,
+    slidesPerView: 7,
+    freeMode: true,
+    loop: true,
+    spaceBetween: 0,
+    freeMode: true,
+    speed: 6500,
+    autoplay: {
+        delay: 0,
+    },
+});
+
+const swiper2 = new Swiper('.swiper2', {
+    slidesPerView: 2,
     freeMode: true,
     loop: true,
     spaceBetween: 44,
